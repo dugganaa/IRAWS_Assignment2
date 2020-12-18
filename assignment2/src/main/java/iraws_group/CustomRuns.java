e000d6e4f26b274c216597290291ed0c216c433e
Aaron Duggan <dugganaa@tcd.ie>
2020-12-18 14:51:12 +0000

    Fixed lambda increment for jm custom runs

--- assignment2/src/main/java/iraws_group/CustomRuns.java
+++ assignment2/src/main/java/iraws_group/CustomRuns.java
@@ -3,7 +3,7 @@ public class CustomRuns {
     private static final float LAMBDA_INCREMENTS = 0.05f;
     public static void main(String[] args) {
         //JM
-        for (float lambda = 0f; lambda <= 1.0f; lambda += LAMBDA_INCREMENTS) {
+        for (float lambda = LAMBDA_INCREMENTS; lambda <= 1.0f; lambda += LAMBDA_INCREMENTS) {
             System.out.println("Running jm: lambda: " + Float.toString(lambda));
             Index.main(new String[]{ "1", Integer.toString(Constants.SimilarityClasses.LMJelinekMercer.ordinal()), Float.toString(lambda)});
             Search.main(new String[]{ "2", Integer.toString(Constants.SimilarityClasses.LMJelinekMercer.ordinal()), Float.toString(lambda), "jm-" + Float.toString(lambda) + ".txt"});
